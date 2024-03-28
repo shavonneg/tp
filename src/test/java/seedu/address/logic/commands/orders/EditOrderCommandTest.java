7e515ee91a1af3030148caad98b7c7267bc04683
shavonneg <>
2024-03-29 01:56:21 +0800

    Add EditOrderCommandTest.java
    Add tests for EditOrderCommand

--- src/test/java/seedu/address/logic/commands/orders/EditOrderCommandTest.java
+++ src/test/java/seedu/address/logic/commands/orders/EditOrderCommandTest.java
@@ -1,11 +1,12 @@
-package seedu.address.logic.commands;
+package seedu.address.logic.commands.orders;
 
 import static java.util.Objects.requireNonNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -19,41 +20,50 @@ import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.commands.orders.AddOrderCommand;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
+import seedu.address.model.UserPrefs;
 import seedu.address.model.order.Order;
 import seedu.address.model.person.Person;
+import seedu.address.testutil.EditOrderDescriptorBuilder;
 import seedu.address.testutil.OrderBuilder;
 import seedu.address.testutil.PersonBuilder;
 
-public class AddOrderCommandTest {
+public class EditOrderCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
-    public void execute_orderAcceptedByModel_addSuccessful() throws Exception {
+    public void execute_editOrderByModel_editSuccessful() throws Exception {
+        Index targetIndex = INDEX_FIRST_PERSON;
         PersonBuilder personBuilder = new PersonBuilder();
         Person person = personBuilder.build();
-        OrderBuilder builder = new OrderBuilder();
-        Order order = builder.build();
-        ModelStubAcceptingOrderAdded modelStub = new ModelStubAcceptingOrderAdded(order, person);
+        Order originalOrder = model.getFilteredOrderList().get(targetIndex.getZeroBased());
+        Order editedOrder = new OrderBuilder().withStatus("completed").build();
+        EditOrderCommand.EditOrderDescriptor descriptor = new EditOrderDescriptorBuilder(editedOrder).build();
+        EditOrderCommand editOrderCommand = new EditOrderCommand(targetIndex, descriptor);
 
-        Index targetIndex = INDEX_FIRST_PERSON;
-        CommandResult commandResult = new AddOrderCommand(targetIndex, order).execute(modelStub);
-        assertEquals(1, modelStub.getFilteredOrderList().size());
+        String expectedMessage = String.format(EditOrderCommand.MESSAGE_EDIT_ORDER_SUCCESS, editedOrder);
+
+        ModelStubEditingOrder modelStub = new ModelStubEditingOrder(originalOrder, person);
+
+        assertCommandSuccess(editOrderCommand, model, expectedMessage, modelStub);
     }
 
     @Test
     public void execute_orderAcceptedByModel_indexError() throws Exception {
+        OrderBuilder orderBuilder = new OrderBuilder();
+        Order order = orderBuilder.build();
         PersonBuilder personBuilder = new PersonBuilder();
         Person person = personBuilder.build();
-        OrderBuilder builder = new OrderBuilder();
-        Order order = builder.build();
-        ModelStubAcceptingOrderAdded modelStub = new ModelStubAcceptingOrderAdded(order, person);
+        ModelStubEditingOrder modelStub = new ModelStubEditingOrder(order, person);
+
         Index targetIndex = INDEX_THIRD_PERSON;
         assertThrows(CommandException.class, () -> new AddOrderCommand(targetIndex, order).execute(modelStub));
     }
-
 
     /**
      * A default model stub that have all of the methods failing.
@@ -159,11 +169,11 @@ public class AddOrderCommandTest {
     /**
      * A Model stub that always accepts the order being added.
      */
-    private class ModelStubAcceptingOrderAdded extends AddOrderCommandTest.ModelStub {
+    private class ModelStubEditingOrder extends seedu.address.logic.commands.orders.EditOrderCommandTest.ModelStub {
         private Order order;
         private Person person;
 
-        ModelStubAcceptingOrderAdded(Order order, Person person) {
+        ModelStubEditingOrder(Order order, Person person) {
             requireNonNull(order);
             this.order = order;
             this.person = person;
@@ -182,6 +192,12 @@ public class AddOrderCommandTest {
         }
 
         @Override
+        public void setPersonAndDeleteOrder(Person target, Person editedPerson, Order order) {
+            requireAllNonNull(target, editedPerson, order);
+            this.person = editedPerson;
+        }
+
+        @Override
         public ObservableList<Person> getFilteredPersonList() {
             List<Person> sampleList = new ArrayList<>();
             sampleList.add(this.person);
@@ -191,8 +207,22 @@ public class AddOrderCommandTest {
 
         @Override
         public ObservableList<Order> getFilteredOrderList() {
+            List<Order> sampleList = new ArrayList<>();
+            sampleList.add(this.order);
+            ObservableList<Order> orderList = FXCollections.observableArrayList(sampleList);
+            return orderList;
+        }
+
+        private ObservableList<Order> getOrderList() {
             ObservableList<Order> orderList = FXCollections.observableArrayList(this.person.getOrders());
             return orderList;
+        }
+
+        @Override
+        public ReadOnlyAddressBook getAddressBook() {
+            AddressBook addressBook = new AddressBook();
+            addressBook.addPerson(this.person);
+            return addressBook;
         }
 
         @Override
