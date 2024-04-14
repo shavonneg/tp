a0820d8d4ecf3ccfb196347e3423a2956fee83ec
kailinteoo <e0959354@u.nus.edu>
2024-04-14 12:31:58 +0800

    Fixing bookkeeper.json error issue

--- src/main/java/seedu/address/logic/commands/clients/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/clients/AddCommand.java
@@ -1,5 +1,12 @@
 package seedu.address.logic.commands.clients;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.Command;
@@ -7,9 +14,6 @@ import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
-
-import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.*;
 
 /**
  * Adds a client to bookkeeper.
